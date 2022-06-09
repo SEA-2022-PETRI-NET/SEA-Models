2c8e586825a33566e3ed399419afb92a8bcbc4da
jfriisKU <xdr476@alumni.ku.dk>
2022-06-09 13:57:18 +0200

    Fix merge

--- PetriNet/Place.cs
+++ PetriNet/Place.cs
@@ -10,20 +10,14 @@ public class Place
     public int PlaceId { get; set; }
     public string? Name { get; set; }
     
-<<<<<<< HEAD:Domain/Model/PetriNet/Place.cs
-=======
     
->>>>>>> 83a61c487b68da0ef189786cd599f69771a62758:PetriNet/Place.cs
     [JsonIgnore]
     public int? PetriNetId { get; set; }
     [JsonIgnore]
     public PetriNet? PetriNet { get; set; }
     
-<<<<<<< HEAD:Domain/Model/PetriNet/Place.cs
     public int? maxAge { get; set; }
     
-=======
->>>>>>> 83a61c487b68da0ef189786cd599f69771a62758:PetriNet/Place.cs
     public bool? isUrgent { get; set; }
 
     // We should either you one or the other of these two fields.
